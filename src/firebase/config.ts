b4afa60dd9dd89e86c4634605625b11fe3fcd5c0
darta com <awegvg@gmail.com>
2026-02-05 13:34:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4179220113-a674c",
+  "appId": "1:971715002747:web:be43f46147fd4738b37dde",
+  "apiKey": "AIzaSyBVuF0uhTgN8GMv5OAeaxrTWJoubnfnhCQ",
+  "authDomain": "studio-4179220113-a674c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "971715002747"
+};
